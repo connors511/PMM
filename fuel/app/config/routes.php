8b9e20ba50087a86bf31a14f10fcaaefb91063bf
Matthias Larsen <matthias@Serenity>
2012-05-12 21:20:39 +0200

    Removed index.php from config, and added rule for pagination

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -2,6 +2,6 @@
 return array(
 	'_root_'  => 'welcome/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
-	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+	'admin/scan/(:num)' => 'admin/scan/$1',
+	'admin/(:segment)(/:num)' => 'admin/$1/$2'
 );
