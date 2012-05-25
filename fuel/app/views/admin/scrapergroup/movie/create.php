4f85474fbfcd1044f78a0ab56c06a50b49245445
Matthias Larsen <matthias@Serenity>
2012-05-26 01:10:54 +0200

    Removing old scrapergroup

--- fuel/app/views/admin/scrapergroup/movie/create.php
+++
@@ -1,7 +0,0 @@
-<h2>New Movie scrapergroup</h2>
-<br>
-
-<?php echo render('admin/scrapergroup/movie/_form'); ?>
-
-
-<p><?php echo Html::anchor('admin/scrapergroup/movie', 'Back'); ?></p>
--- fuel/app/views/admin/scrapergroup/movie/edit.php
+++
@@ -1,7 +0,0 @@
-<h2>Editing Movie scrapergroup</h2>
-<br>
-
-<?php echo render('admin/scrapergroup/movie/_form'); ?>
-<p>
-	<?php echo Html::anchor('admin/scrapergroup/movie/view/'.$scrapergroup_movie->id, 'View'); ?> |
-	<?php echo Html::anchor('admin/scrapergroup/movie', 'Back'); ?></p>
